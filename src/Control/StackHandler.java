cab303ca174cd4436d4ad7eff02d5ad6d25c2f84
yummiejan <janpaul.albers@gmail.com>
2016-11-28 11:05:05 +0100

    Updates

--- src/Control/StackHandler.java
+++ src/Control/StackHandler.java
@@ -1,9 +1,40 @@
 package Control;
 
+import Model.*;
+import View.*;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+
 /**
  * Created by gast05 on 28.11.2016.
  */
-public class StackHandler {
+public class StackHandler implements InteractableObject {
     private Stack<Card> stackOfUnrevealed;
     private Stack<Card> stackOfRevealed;
+
+    @Override
+    public void keyPressed(int key) {
+
+    }
+
+    @Override
+    public void keyReleased(int key) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void draw(DrawingPanel dp, Graphics2D g2d) {
+
+    }
+
+    @Override
+    public void update(double dt) {
+
+    }
 }
--- src/Model/Card.java
+++ src/Model/Card.java
@@ -1,7 +1,30 @@
 package Model;
 
+import View.DrawableObject;
+import View.DrawingPanel;
+
+import java.awt.*;
+
 /**
  * Created by gast05 on 28.11.2016.
  */
-public class Card {
+public class Card implements DrawableObject {
+
+    private Suits suit;
+    private Value value;
+
+    public Card(Suits suit, Value value){
+        this.suit = suit;
+        this.value = value;
+    }
+
+    @Override
+    public void draw(DrawingPanel dp, Graphics2D g2d) {
+
+    }
+
+    @Override
+    public void update(double dt) {
+
+    }
 }
