7eaea1600cab13698cae625aaee661f7aa9f1b58
yummiejan <janpaul.albers@gmail.com>
2016-11-28 11:20:34 +0100

    Updates

--- src/Model/Card.java
+++ src/Model/Card.java
@@ -4,6 +4,8 @@ import View.DrawableObject;
 import View.DrawingPanel;
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 
 /**
  * Created by gast05 on 28.11.2016.
@@ -12,10 +14,16 @@ public class Card implements DrawableObject {
 
     private Suits suit;
     private Value value;
+    private Rectangle2D.Double rectangle;
+
 
     public Card(Suits suit, Value value){
         this.suit = suit;
         this.value = value;
+    }
+
+    private void drawCard(){
+
     }
 
     @Override
