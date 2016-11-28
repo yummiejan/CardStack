1a7feac7373f263a35fe6dcd818ca4cf3b5bdc3b
yummiejan <janpaul.albers@gmail.com>
2016-11-28 10:54:28 +0100

    StackHanlder Updates

--- src/Control/StackHandler.java
+++ src/Control/StackHandler.java
@@ -4,4 +4,6 @@ package Control;
  * Created by gast05 on 28.11.2016.
  */
 public class StackHandler {
+    private Stack<Card> stackOfUnrevealed;
+    private Stack<Card> stackOfRevealed;
 }
--- src/Model/Suits.java
+++ src/Model/Suits.java
@@ -4,4 +4,5 @@ package Model;
  * Created by gast05 on 28.11.2016.
  */
 public enum Suits {
+    SPADES, CLUBS, DIAMONDS, HEARTS
 }
