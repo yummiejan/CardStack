de65d64b000d1a58bae0801fc4753b033d5e5634
yummiejan <janpaul.albers@gmail.com>
2016-11-28 10:38:16 +0100

    Alle nötigen Klassen erstellt

--- src/Control/MainController.java
+++ src/Control/MainController.java
@@ -1,7 +1,12 @@
 package Control;
 
+import View.MainFrame;
+
 /**
  * Created by gast05 on 28.11.2016.
  */
 public class MainController {
+    public MainController(MainFrame mainFrame){
+
+    }
 }
---
+++ src/MainProgram.java
@@ -0,0 +1,22 @@
+import Control.*;
+import View.MainFrame;
+
+import java.awt.*;
+/**
+ * Created by gast05 on 28.11.2016.
+ */
+public class MainProgram {
+    public static void main (String[] args){
+        EventQueue.invokeLater(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        MainProgram.setup();
+                    }
+                });
+    }
+
+    private static void setup(){
+        new MainController(new MainFrame("Card stack",50,50,1100,900));
+    }
+}
