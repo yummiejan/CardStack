8e9edbb1a9a284febb596908417b5bb3d624affd
Jan <janpaul.albers@gmail.com>
2016-11-29 17:29:58 +0100

    Updates

--- src/Model/Card.java
+++ src/Model/Card.java
@@ -12,6 +12,7 @@ import java.awt.geom.Rectangle2D;
  */
 public class Card implements DrawableObject {
 
+    private double xPos,yPos,width,height;
     private Suits suit;
     private Value value;
     private Rectangle2D.Double rectangle;
@@ -20,6 +21,11 @@ public class Card implements DrawableObject {
     public Card(Suits suit, Value value){
         this.suit = suit;
         this.value = value;
+        xPos = 100;
+        yPos = 100;
+        width = 50;
+        height = 100;
+        rectangle = new Rectangle2D.Double(xPos,yPos,width,height);
     }
 
     private void drawCard(){
@@ -28,7 +34,11 @@ public class Card implements DrawableObject {
 
     @Override
     public void draw(DrawingPanel dp, Graphics2D g2d) {
-
+        g2d.setColor(new Color(255,255, 255));
+        g2d.fill(rectangle);
+        g2d.setColor(new Color(0,0,0));
+        g2d.draw(rectangle);
+        rectangle.setFrame(xPos,yPos,width,height);
     }
 
     @Override
